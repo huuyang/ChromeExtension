56129122e079f4672d9e07b207322f7e1d66135c
huuyang <46680598+huuyang@users.noreply.github.com>
2020-05-25 21:02:13 +0800

    截图&下载 间隔5秒 重复20次
    chrome.tabs.captureVisibleTab
    chrome.downloads.download
    setTimeout
    alert

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//     new Notification('比赛开始啦', {icon: 'live.png',body: 'From background.js'});
-// });
+let count = 0;
+function backgroundFunction() {
+	if(count++ < 20){
+		chrome.tabs.captureVisibleTab({format:'jpeg', quality:100}, function(dataUrl){
+    		chrome.downloads.download({
+    			url: dataUrl, 
+    			filename: 'liveShot.jpg', 
+    			conflictAction: 'uniquify', 
+    			saveAs: false
+    		});
+    	});
+    	setTimeout(function(){backgroundFunction()}, 5000);
+	}else{
+		alert('截图完成');
+	}
 
-new Notification('比赛开始啦', {icon: 'live.png',body: 'From background.js'});
+    // chrome.desktopCapture.chooseDesktopMedia(["screen", "window", "tab"], function(id) {
+    // 	alert('开始截图!');
+    // })
+}

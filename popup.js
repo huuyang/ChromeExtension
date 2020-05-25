b41c9e64aad59f05711514425a2409c5dd5cce92
huuyang <46680598+huuyang@users.noreply.github.com>
2020-05-25 21:08:32 +0800

    点击popup里的图片→去background.js里执行函数

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
-new Notification('比赛开始啦~', {icon: 'live.png',body: 'From popup.js'});
+//chrome.tabs.captureVisibleTab(function(dataUrl){document.getElementById('target').src=dataUrl});
 
-chrome.tabs.captureVisibleTab(function(dataUrl){document.getElementById('target').src=dataUrl});
+document.getElementById('target').addEventListener('click', function() {
+    chrome.runtime.getBackgroundPage(function(backgroundPage){
+    	backgroundPage.backgroundFunction()
+    })
+});
